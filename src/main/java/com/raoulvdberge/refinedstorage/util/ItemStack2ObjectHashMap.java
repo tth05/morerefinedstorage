639d61126e38cb2f75e3f9f5dc14cbde39eeb016
tth05 <36999320+tth05@users.noreply.github.com>
2020-12-02 22:37:09 +0100

    switch to fastutil custom hashmap

--- src/main/java/com/raoulvdberge/refinedstorage/util/ItemStack2ObjectHashMap.java
+++ src/main/java/com/raoulvdberge/refinedstorage/util/ItemStack2ObjectHashMap.java
@@ -1,24 +1,24 @@
 package com.raoulvdberge.refinedstorage.util;
 
-import gnu.trove.map.hash.TCustomHashMap;
-import gnu.trove.strategy.HashingStrategy;
+import it.unimi.dsi.fastutil.Hash;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenCustomHashMap;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 import java.util.Objects;
 
-public class ItemStack2ObjectHashMap<V> extends TCustomHashMap<ItemStack, V> {
+public class ItemStack2ObjectHashMap<V> extends Object2ObjectOpenCustomHashMap<ItemStack, V> {
 
     public ItemStack2ObjectHashMap() {
         super(ItemStackHashingStrategy.INSTANCE);
     }
 
-    private static class ItemStackHashingStrategy implements HashingStrategy<ItemStack> {
+    private static class ItemStackHashingStrategy implements Hash.Strategy<ItemStack> {
 
         private static final ItemStackHashingStrategy INSTANCE = new ItemStackHashingStrategy();
 
         @Override
-        public int computeHashCode(ItemStack object) {
+        public int hashCode(ItemStack object) {
             NBTTagCompound nbt = object.getTagCompound();
 
             int hashCode = 31 + Boolean.hashCode(object.isEmpty());
@@ -30,10 +30,12 @@ public class ItemStack2ObjectHashMap<V> extends TCustomHashMap<ItemStack, V> {
 
         @Override
         public boolean equals(ItemStack o1, ItemStack o2) {
-            return o1.isEmpty() == o2.isEmpty() &&
-                    o1.getItemDamage() == o2.getItemDamage() &&
-                    o1.getItem().equals(o2.getItem()) &&
-                    Objects.equals(o1.getTagCompound(), o2.getTagCompound());
+            return (o1 == null && o2 == null) ||
+                    (o1 != null && o2 != null &&
+                            o1.isEmpty() == o2.isEmpty() &&
+                            o1.getItemDamage() == o2.getItemDamage() &&
+                            o1.getItem().equals(o2.getItem()) &&
+                            Objects.equals(o1.getTagCompound(), o2.getTagCompound()));
         }
     }
 }
