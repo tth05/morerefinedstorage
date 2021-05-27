tth05/morerefinedstorage

commit a1d16eaeb93966faee0a5f2b4bd3d3ed4dedc247

fix comparer not treating null and empty compound equally

src/main/java/com/raoulvdberge/refinedstorage/apiimpl/util/Comparer.java
@@ -36,8 +36,16 @@ public class Comparer implements IComparer {
             return false;
         }
 
-        if ((flags & COMPARE_NBT) == COMPARE_NBT && !ItemStack.areItemStackTagsEqual(left, right)) {
-            return false;
+        if ((flags & COMPARE_NBT) == COMPARE_NBT) {
+            boolean leftTagEmpty = left.getTagCompound() == null || left.getTagCompound().isEmpty();
+            boolean rightTagEmpty = right.getTagCompound() == null || right.getTagCompound().isEmpty();
+            if (leftTagEmpty) {
+                if (!rightTagEmpty)
+                    return false;
+            } else {
+                if (!left.getTagCompound().equals(right.getTagCompound()) || !left.areCapsCompatible(right))
+                    return false;
+            }
         }
 
         if ((flags & COMPARE_DAMAGE) == COMPARE_DAMAGE && left.getItemDamage() != right.getItemDamage()) {

commit 4562ee033a190dee9b579041d875bdb26d958cf5

Fixed items with different capabilities being stacked together (again)
Broke in a1d16eaeb93966faee0a5f2b4bd3d3ed4dedc247

src/main/java/com/raoulvdberge/refinedstorage/apiimpl/util/Comparer.java
@@ -40,7 +40,7 @@ public class Comparer implements IComparer {
             boolean leftTagEmpty = left.getTagCompound() == null || left.getTagCompound().isEmpty();
             boolean rightTagEmpty = right.getTagCompound() == null || right.getTagCompound().isEmpty();
             if (leftTagEmpty) {
-                if (!rightTagEmpty)
+                if (!rightTagEmpty || !left.areCapsCompatible(right))
                     return false;
             } else {
                 if (!left.getTagCompound().equals(right.getTagCompound()) || !left.areCapsCompatible(right))

src/test/java/morerefinedstorage/comparer/ComparerTest.java
@@ -1,0 +1,85 @@
+package morerefinedstorage.comparer;
+
+import com.raoulvdberge.refinedstorage.api.util.IComparer;
+import com.raoulvdberge.refinedstorage.apiimpl.API;
+import morerefinedstorage.MinecraftForgeTest;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ComparerTest implements MinecraftForgeTest {
+
+    private final IComparer comparer = API.instance().getComparer();
+
+    @Test
+    public void testItemsEqual() {
+        ItemStack stack1 = new ItemStack(Items.APPLE, 456567);
+        ItemStack stack2 = new ItemStack(Items.APPLE, 12);
+
+        assertTrue(comparer.isEqualNoQuantity(stack1, stack2));
+        assertFalse(comparer.isEqual(stack1, stack2, IComparer.COMPARE_NBT | IComparer.COMPARE_DAMAGE | IComparer.COMPARE_QUANTITY));
+    }
+
+    @Test
+    public void testDamageEqual() {
+        ItemStack stack1 = new ItemStack(Items.DIAMOND_AXE);
+        ItemStack stack2 = new ItemStack(Items.DIAMOND_AXE);
+
+        assertTrue(comparer.isEqual(stack1, stack2));
+
+        stack1.setItemDamage(1);
+
+        assertFalse(comparer.isEqual(stack1, stack2));
+        assertTrue(comparer.isEqual(stack1, stack2, IComparer.COMPARE_NBT | IComparer.COMPARE_QUANTITY));
+
+        stack2.setItemDamage(1);
+
+        assertTrue(comparer.isEqual(stack1, stack2));
+    }
+
+    @Test
+    public void testNBTEqual() {
+        ItemStack stack1 = new ItemStack(Items.DIAMOND_AXE);
+        NBTTagCompound compound1 = new NBTTagCompound();
+        compound1.setString("t", "1");
+        stack1.setTagCompound(compound1);
+        ItemStack stack2 = new ItemStack(Items.DIAMOND_AXE);
+        NBTTagCompound compound2 = new NBTTagCompound();
+        compound2.setString("t", "1");
+
+        assertFalse(comparer.isEqual(stack1, stack2));
+        assertTrue(comparer.isEqual(stack1, stack2, IComparer.COMPARE_DAMAGE | IComparer.COMPARE_QUANTITY));
+
+        stack2.setTagCompound(compound2);
+
+        assertTrue(comparer.isEqual(stack1, stack2));
+        assertTrue(comparer.isEqual(stack1, stack2, IComparer.COMPARE_DAMAGE | IComparer.COMPARE_QUANTITY));
+
+        stack1.setTagCompound(new NBTTagCompound());
+
+        assertFalse(comparer.isEqual(stack1, stack2));
+        assertTrue(comparer.isEqual(stack1, stack2, IComparer.COMPARE_DAMAGE | IComparer.COMPARE_QUANTITY));
+
+        stack2.setTagCompound(null);
+
+        assertTrue(comparer.isEqual(stack1, stack2));
+        assertTrue(comparer.isEqual(stack1, stack2, IComparer.COMPARE_DAMAGE | IComparer.COMPARE_QUANTITY));
+
+        stack2.setTagCompound(new NBTTagCompound());
+
+        assertTrue(comparer.isEqual(stack1, stack2));
+        assertTrue(comparer.isEqual(stack1, stack2, IComparer.COMPARE_DAMAGE | IComparer.COMPARE_QUANTITY));
+
+        stack1.setTagCompound(null);
+        stack2.setTagCompound(null);
+
+        assertTrue(comparer.isEqual(stack1, stack2));
+        assertTrue(comparer.isEqual(stack1, stack2, IComparer.COMPARE_DAMAGE | IComparer.COMPARE_QUANTITY));
+    }
+
+    //TODO: test capabilities somehow
+}