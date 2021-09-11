9eba1b0b5f14bd3d32653e5877ec3d47ced800ab
tth05 <36999320+tth05@users.noreply.github.com>
2021-09-11 13:22:25 +0200

    Fixed item in autocrafting settings gui being extractable

--- src/main/java/com/raoulvdberge/refinedstorage/container/slot/SlotDisabled.java
+++ src/main/java/com/raoulvdberge/refinedstorage/container/slot/SlotDisabled.java
@@ -1,5 +1,6 @@
 package com.raoulvdberge.refinedstorage.container.slot;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 
@@ -8,6 +9,11 @@ import javax.annotation.Nonnull;
 public class SlotDisabled extends SlotBase {
     public SlotDisabled(IItemHandler itemHandler, int inventoryIndex, int x, int y) {
         super(itemHandler, inventoryIndex, x, y);
+    }
+
+    @Override
+    public boolean canTakeStack(EntityPlayer playerIn) {
+        return false;
     }
 
     @Override
--- src/main/java/com/raoulvdberge/refinedstorage/container/slot/filter/SlotFilterFluidDisabled.java
+++ src/main/java/com/raoulvdberge/refinedstorage/container/slot/filter/SlotFilterFluidDisabled.java
@@ -1,6 +1,7 @@
 package com.raoulvdberge.refinedstorage.container.slot.filter;
 
 import com.raoulvdberge.refinedstorage.inventory.fluid.FluidInventory;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -12,6 +13,11 @@ public class SlotFilterFluidDisabled extends SlotFilterFluid {
 
     public SlotFilterFluidDisabled(FluidInventory inventory, int inventoryIndex, int x, int y) {
         super(inventory, inventoryIndex, x, y);
+    }
+
+    @Override
+    public boolean canTakeStack(EntityPlayer playerIn) {
+        return false;
     }
 
     @Override
